8c02c12c605bd22cf28680c6b0d44e789621baff
Kagisho96 <86986879+Kagisho96@users.noreply.github.com>
2023-02-28 19:39:10 +0200

    Add files via upload

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,26 @@
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
+
+const Testimonials = () => (
+  <section id="partners" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={styles.heading2}>   
+        Dedication and Kind regards <br className="sm:block hidden" />from folks below
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit blanditiis quae voluptas unde vel ea, commodi ipsum quam itaque excepturi libero quibusdam similique, soluta non consectetur minus quis dolorem autem?
+        </p>
+      </div>
+    </div>
+
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
+    </div>
+  </section>
+);
+
+export default Testimonials;
---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star,  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "dashboard",
+    title: "Dashboard",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "partners",
+    title: "Partners",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "xxxxxxxxx",
+    content:
+      "Self confidence is a super power. Once you start to believe in yourself, magic starts happening.",
+  },
+  {
+    id: "feature-2",
+    icon: star,
+    title: "xxxxxxxx",
+    content:
+      "“As soon as you start to pursue a dream, your life wakes up and everything has meaning.”",
+  },
+  {
+    id: "feature-3",
+    icon: star,
+    title: "xxxxxxxxx",
+    content:
+      "“Be a dreamer. If you don’t know how to dream, you’re dead.” ",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Knowledge is the only tool that will take you wherever you wish, but it will not produce results without applying that knowledge.",
+    name: "Pitsi Kagisho",
+    title: "Founder",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Knowledge is the only tool that will take you wherever you wish, but it will not produce results without applying that knowledge.",
+    name: "Mphaka Johannes",
+    title: "Cousin",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Knowledge is the only tool that will take you wherever you wish, but it will not produce results without applying that knowledge.",
+    name: "Pitsi Dimpho",
+    title: "Brother",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "0721518903",
+    value: "PHONE",
+  },
+  {
+    id: "stats-2",
+    title: "kagishopitsi@gmail.com",
+    value: "EMAIL",
+  },
+  {
+    id: "stats-3",
+    title: "PITSI",
+    value: "K M",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.pitsikagisho.com/content/",
+      },
+      // {
+      //   name: "How it Works",
+      //   link: "https://www.pitsikagisho.com/how-it-works/",
+      // },
+      {
+        name: "Create",
+        link: "https://www.pitsikagisho.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.pitsikagisho.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.pitsikagisho.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.pitsikagisho.com/help-center/",
+      },
+      // {
+      //   name: "Partners",
+      //   link: "https://www.pitsikagisho.com/partners/",
+      // },
+      {
+        name: "Suggestions",
+        link: "https://www.pitsikagisho.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.pitsikagisho.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.pitsikagisho.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.pitsikagisho.com/our-partner/",
+      },
+      {
+        name: "Collaborate/Join",
+        link: "https://www.pitsikagisho.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
