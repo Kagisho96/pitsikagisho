a76ec5f67599a8e0af99d5b5be0f5a8c94359efc
kagisho96 <kagishopitsi@gmail.com>
2024-02-11 16:00:15 +0200

    responsive

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,5 +1,6 @@
-import styles from "../style";
 import Button from "./Button";
+import styles from "../style";
+
 import {
   BuildingOffice2Icon,
   EnvelopeIcon,
@@ -8,66 +9,38 @@ import {
 
 const CTA = () => (
   <section id="contacts" className="sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow p-8">
-    <div className="">
-      <div className="flex items-center justify-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-100">
-          Get in touch
-        </h2>
+  <div className="">
+    <div className="flex items-center justify-center">
+      <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight text-gradient">
+        Get in touch
+      </h2>
+    </div>
+
+    <div className="flex flex-col sm:flex-row w-full mt-4 justify-between">
+      <div className="flex items-center mb-4 sm:mb-0">
+        <BuildingOffice2Icon className="h-7 w-6 text-gray-200" aria-hidden="true" />
+        <p className="text-gradient font-poppins font-semibold text-sm sm:text-base lg:text-lg text-gray-200 ml-3">
+          190 Scheiding St, <br className="sm:hidden" /> Pretoria Central, Pretoria, 0002
+        </p>
       </div>
 
-      <div className="flex w-full mt-4 justify-between">
-        <div className="flex">
-          <dt className="flex-none">
-            <span className="sr-only">Address</span>
-            <BuildingOffice2Icon
-              className="h-7 w-6 text-gray-200"
-              aria-hidden="true"
-            />
-          </dt>
-          <dd>
-            <p className="font-poppins font-semibold xs:text-[15px] text-[10px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3">
-              190 Scheiding St, 
-              <br />
-              Pretoria Central, Pretoria, 0002
-            </p>
-          </dd>
-        </div>
+      <div className="flex items-center mb-4 sm:mb-0">
+        <PhoneIcon className="h-7 w-6 text-gray-200" aria-hidden="true" />
+        <a className="text-gradient font-poppins font-semibold text-sm sm:text-base lg:text-lg text-gray-200 ml-3" href="tel:+27721518903">
+          +27 72 151 8903
+        </a>
+      </div>
 
-        <div className="flex">
-          <dt className="flex-none">
-            <span className="sr-only">Telephone</span>
-            <PhoneIcon className="h-7 w-6 text-gray-200" aria-hidden="true" />
-          </dt>
-          <dd>
-            <a
-              className="font-poppins font-semibold xs:text-[15px] text-[10px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3"
-              href="0721518903"
-            >
-              +27 72 151 8903
-            </a>
-          </dd>
-        </div>
-
-        <div className="flex">
-          <dt className="flex-none">
-            <span className="sr-only">Email</span>
-            <EnvelopeIcon
-              className="h-7 w-6 text-gray-200"
-              aria-hidden="true"
-            />
-          </dt>
-          <dd>
-            <a
-              className="font-poppins font-semibold xs:text-[15px] text-[10px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3"
-              href="kagishopitsi@gmail.com"
-            >
-              kagishopitsi@gmail.com
-            </a>
-          </dd>
-        </div>
+      <div className="flex items-center">
+        <EnvelopeIcon className="h-7 w-6 text-gray-200" aria-hidden="true" />
+        <a className="text-gradient font-poppins font-semibold text-sm sm:text-base lg:text-lg text-gray-200 ml-3" href="mailto:kagishopitsi@gmail.com">
+          kagishopitsi@gmail.com
+        </a>
       </div>
     </div>
-  </section>
+  </div>
+</section>
+
 );
 
 export default CTA;
